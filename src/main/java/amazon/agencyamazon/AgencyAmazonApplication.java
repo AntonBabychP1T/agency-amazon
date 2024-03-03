59fb2419ea0df0d43dd6c0a447ff7a836d01182f
Anton Babych <antonbabi13@gmail.com>
2024-03-03 18:29:00 +0200

    Initial commit

---
+++ src/main/java/amazon/agencyamazon/AgencyAmazonApplication.java
@@ -0,0 +1,13 @@
+package amazon.agencyamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyAmazonApplication.class, args);
+	}
+
+}
